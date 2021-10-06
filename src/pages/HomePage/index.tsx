2177f37c620cffa82fb045cb9046843c1b71597b
ingridsj <ingridsjalmeida@gmail.com>
2021-10-06 13:40:34 -0300

    fix: renamed component

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,15 +1,20 @@
 import { imagePokemon } from "../../assets/images";
 import { question } from "../../assets/icons";
+import { GamePage } from "../GamePage/index";
 
 import "./index.scss";
+import { Link } from "react-router-dom";
 
-export function Home() {
+export function HomePage() {
   return (
     <main className="main-home">
       <h1>Quem é esse</h1>
       <img src={imagePokemon} alt="Pokémon" />
       <img src={question} alt="Interrogação" />
-      <button>Começar</button>
+
+      <Link to="game-page">
+        <button>Começar</button>
+      </Link>
     </main>
   );
 }
